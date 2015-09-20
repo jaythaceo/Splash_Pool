e42cc79d1e016104ddaec42ad72763fc261eed1b
jaythaceo <jaythaceo@gmail.com>
2015-09-19 23:34:05 -0400

    [chore] Added Static Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
+  get 'static_pages/services'
+
+  get 'static_pages/portfolio'
+
+  get 'static_pages/contact'
+
 end
